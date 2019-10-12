096b76c5620f7bd4acdc212c9f1d4c5d1f67bd66
saids <saidsamadli@gmail.com>
2019-10-12 00:24:09 -0400

    misspell

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -8,7 +8,7 @@ import { Product } from './product';
 @Injectable()
 export class ProductService {
   private _albumUrl =   "../assets/album.json";
-  private _productUrl = "../assets/products.json";
+  private _productsUrl = "../assets/products.json";
 
   constructor(private _http: Http) {}
 
@@ -20,7 +20,7 @@ export class ProductService {
 
   getProducts(): Observable<Product[]> {
     return this._http
-      .get(this._productUrl)
+      .get(this._productsUrl)
       .map(response => <Product[]>response.json());
   }
 }
